adf8b2aeede72bb73a226eec4ed6c05e23f0d0d0
JupiterPi <jupiterpi@outlook.de>
2021-07-28 08:15:20 +0200

    implemented simple Discord bot for logging

---
+++ src/main/java/jupiterpi/healthcluster/bot/Bot.java
@@ -0,0 +1,37 @@
+package jupiterpi.healthcluster.bot;
+
+import jupiterpi.healthcluster.GlobalPreferences;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.TextChannel;
+
+import javax.security.auth.login.LoginException;
+
+public class Bot {
+    private JDA jda;
+    private Guild guild;
+    private TextChannel loggingChannel;
+
+    private String selfName = "unknown";
+
+    public Bot() {
+        try {
+            String token = GlobalPreferences.get("bot-token");
+            jda = JDABuilder.createDefault(token)
+                    .build();
+            jda.awaitReady();
+
+            guild = jda.getGuildById(GlobalPreferences.get("bot-guild"));
+            loggingChannel = guild.getTextChannelById(GlobalPreferences.get("bot-logging-channel"));
+
+            selfName = GlobalPreferences.get("self-name");
+        } catch (LoginException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void log(String message) {
+        loggingChannel.sendMessage(String.format("[%s] %s", selfName, message)).queue();
+    }
+}
