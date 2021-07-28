219d2417434defc4571c57cfe78fa15debb4b34a
JupiterPi <jupiterpi@outlook.de>
2021-07-28 07:51:06 +0200

    preferences file info

--- src/main/java/jupiterpi/healthcluster/GlobalPreferences.java
+++ src/main/java/jupiterpi/healthcluster/GlobalPreferences.java
@@ -4,7 +4,7 @@ import jupiterpi.healthcluster.preferences.Preferences;
 import jupiterpi.tools.files.Path;
 
 public class GlobalPreferences {
-    private static final Preferences preferences = new Preferences(Path.getRunningDirectory().file("sample-preferences"));
+    private static final Preferences preferences = new Preferences(Path.getRunningDirectory().file("preferences")); // preferences file is hidden from git; use sample-preferences as reference
 
     public static String get(String key) {
         return preferences.get().get(key);
